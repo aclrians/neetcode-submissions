c8bb61e4281ff3c74658d1c7ce38601db9540c14
Adrian <adrianbrusil44@gmail.com>
2026-07-23 12:48:47 -0700

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int calPoints(String[] operations) {
+
+        Stack<Integer> s = new Stack<>();
+
+        for(String ops:operations){
+            if(ops.equals("C")){
+                s.pop();
+            }
+            else if(ops.equals("D")){
+                s.push(2*s.peek());
+            }
+            else if(ops.equals("+")){
+                int top = s.pop(); // Record our top value and get rid of it
+                int val = top + s.peek(); // Add our old top value with the value that comes before it
+                s.push(top);
+                s.push(val);
+
+            } else { 
+                s.push(Integer.parseInt(ops));
+            }
+        }
+
+        int sum = 0;
+        while (s.size() > 0){
+
+         sum += s.pop();
+        }
+
+        return sum;
+        
+    }
+
+}
